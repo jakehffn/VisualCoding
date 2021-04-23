c46d47ef665e00278ab316a95afe55a47e51391c
Jake Hoffman <jaketh19@gmail.com>
2021-04-23 18:18:31 -0400

    Changed method of changing programs

--- src/main.cpp
+++ src/main.cpp
@@ -25,33 +25,36 @@ VisualProgram* program;
 bool init() {
     bool success = true;
 
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
 		success = false;
 
 	} else {
 		// Use OpenGL 3.1 core
-		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
-		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
-		SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-		window = SDL_CreateWindow( "visualCoding", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, render_consts::SCREEN_WIDTH, render_consts::SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+		window = SDL_CreateWindow("visualCoding", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+			render_consts::SCREEN_WIDTH, render_consts::SCREEN_HEIGHT, 
+			SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_INPUT_FOCUS
+		);
 
-		if( window == NULL ) {
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+		if(window == NULL) {
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
 			success = false;
 
 		} else {
-			gContext = SDL_GL_CreateContext( window );
+			gContext = SDL_GL_CreateContext(window);
 
-			if( gContext == NULL ) {
-				printf( "SDL: OpenGL context could not be created!\nSDL Error: %s\n", SDL_GetError() );
+			if(gContext == NULL) {
+				printf("SDL: OpenGL context could not be created!\nSDL Error: %s\n", SDL_GetError());
 				success = false;
 			} else {
 				glewExperimental = GL_TRUE; 
 				GLenum glewError = glewInit();
 				if( glewError != GLEW_OK ) {
-					printf( "GLEW: Error initializing! %s\n", glewGetErrorString( glewError ) );
+					printf("GLEW: Error initializing! %s\n", glewGetErrorString(glewError));
 				}
 
 				// Use Vsync
@@ -73,54 +76,46 @@ bool init() {
 void close() {
 
 	// Destroy window	
-	SDL_DestroyWindow( window );
+	SDL_DestroyWindow(window);
 	window = NULL;
 
 	// Quit SDL subsystems
 	SDL_Quit();
 }
 
-void chooseProgram() {
+void applyProgram(std::string input) {
 
-	std::vector<std::string> programNames {
-		"OpenGLTutorial", 
-		"MultipleObjects"
-	};
+	if (input == "OpenGLTutorial") {
+		program = new OpenGLTutorial();
+	} 
 
-	for (int xx = 0; xx < programNames.size(); xx++) {
-		std::cout << "\n" << xx << " - " << programNames.at(xx);
+	if (input == "MultipleObjects") {
+		program = new MultipleObjects();
 	}
-	
-	std::cout << "\n\nChoose Program: ";
 
-	int input{0};
-
-	std::cin >> input;
-	std::cout << "\n";
-
-	switch (input) {
-		case 0:
-			program = new OpenGLTutorial();
-			break;
-		case 1:
-			program = new MultipleObjects();
-			break;
-		default:
-			break;
-	}
 }
 
 // Parameters necessary for SDL_Main
 int main(int argv, char** args) {
 
-	chooseProgram();
+	std::string input;
+
+	if (argv == 1) {
+		input = std::string("OpenGLTutorial");
+	} else {
+		input = std::string(args[1]);
+	}
+
+	applyProgram(input);
 
 	// Start up SDL and create window
-	if( !init() ) {
+	if(!init()) {
 		
 		printf("Failed to initialize!\n");
 
 	} else {
+		// SDL_SetWindowGrab(window, SDL_TRUE);
+		// SDL_SetWindowInputFocus(window);
 		program->run();
 	}
 
