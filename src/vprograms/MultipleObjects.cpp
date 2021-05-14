23280b53d51a3392d127ee41bdbd8022291d0746
Jake Hoffman <jaketh19@gmail.com>
2021-05-13 23:47:06 -0400

    Changed path speed

--- src/vprograms/MultipleObjects.cpp
+++ src/vprograms/MultipleObjects.cpp
@@ -32,9 +32,9 @@ void MultipleObjects::run() {
 
     int objID = scene->addObject(objPath);
     int instanceID = scene->addInstance(objID, shaderID, glm::vec3(0, 0, 0), glm::vec3(1, 1, 1), glm::vec3(0, 0, 0));
-    int instanceID2 = scene->addInstance(objID, shaderID, glm::vec3(4, 0, 0), glm::vec3(1, 1, 1), glm::vec3(0, 0, 0));
+    int instanceID2 = scene->addInstance(objID, shaderID, glm::vec3(4, 0, 4), glm::vec3(1, 1, 1), glm::vec3(0, 0, 0));
 
-    CirclePath* path = new CirclePath(glm::vec3(0, 0, 0), 8, 5, 0.001);
+    CirclePath* path = new CirclePath(glm::vec3(0, 0, 0), 8, 5, 1);
     PathCameraController* controller2 = new PathCameraController(clock, path);
     scene->addCameraController(controller2);
 
